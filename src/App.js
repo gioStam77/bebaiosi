85ae22463bc6f615361d82bb85e1bf9c10fa2f84
giorgio <giorgio.stam77@gmail.com>
2021-06-01 10:34:12 +0300

    telikoOK

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
+import "./App.css";
+import Bebaiosi from "./components/Bebaiosi";
+import { useReactToPrint } from "react-to-print";
 
 function App() {
+  const [dataloaded, setDataLoaded] = useState([]);
+
+  const componentRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
+
+  function addDataHandler(data) {
+    setDataLoaded(data);
+  }
+
+  console.log(dataloaded.optikos);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Bebaiosi onAddData={addDataHandler} />
+      </div>
+      <button onClick={handlePrint}>print</button>
+      <div className="container" ref={componentRef}>
+        <div className="bebaiosi">
+          <h3>ΑΔΑ¨ΒΕΑ10Ξ7Μ-8Φ6</h3>
+          <h2>ΒΕΒΑΙΩΣΗ</h2>
+          <p>
+            O κάτωθι υπογεγραμμένος οπτικός {dataloaded.optikos + " "}
+            που διατηρώ κατάστημα οπτικών επί της οδού ΤΡΙΟΒΑΣΑΛΟΣ ΜΗΛΟΥ ,
+            βεβαιώνω ότι εκτέλεσα επακριβώς την από {dataloaded.day} ιατρική
+            γνωμάτευση του οφθαλμιάτρου {dataloaded.doctor} για τον ασφαλισμένο{" "}
+            {dataloaded.asfalismenos} με ΑΜΚΑ {dataloaded.amka + " "} και
+            παρέδωσα στον ανωτέρω ασφαλισμένο ΓΥΑΛΙΑ ΟΡΑΣΕΩΣ εκδίδοντας την
+            Απόδειξη Παροχής Υπηρεσιών νούμερο {dataloaded.apodeixi}.<br />
+            Βεβαιώνω ότι το ανωτέρω χορηγούμενο είδος φέρει πιστοποίηση
+            καταλληλότητας CE.
+            <h4 style={{ paddingLeft: "65px" }}>Ο ΟΠΤΙΚΟΣ</h4>
+            <img src="/sfragida.png" alt="σφαραγίδα" width="200" />
+          </p>
+        </div>
+        <div className="apodeixi">
+          <img className="logo" src="/ocellus_logo.png" width="140"></img>
+          <h2>ΑΠΟΔΕΙΞΗ ΕΙΣΠΡΑΞΗΣ</h2>
+          <h4>ΗΜΕΡΟΜΗΝΙΑ {dataloaded.day}</h4>
+          <p>
+            Ο/Η {dataloaded.asfalismenos} ασφαλισμένος του {dataloaded.tameio}
+            κατέβαλε το ποσό των {dataloaded.poso}{" "}
+            €(......................................................................
+            ) για την αγορά:
+            <br />
+            <ul>
+              {dataloaded.selected === "1" && (
+                <li>
+                  Γυαλιά οράσεως μακρινά με σκελετό{" "}
+                  {parseInt(dataloaded.poso) / 2}€ και φακούς
+                  {parseInt(dataloaded.poso) / 2}€
+                </li>
+              )}
+              {dataloaded.selected === "2" && (
+                <li>
+                  Γυαλιά οράσεως κοντινά με σκελετό{" "}
+                  {parseInt(dataloaded.poso) / 2}€ και φακούς
+                  {parseInt(dataloaded.poso) / 2}€
+                </li>
+              )}
+              {dataloaded.selected === "3" && (
+                <li>Φακούς επαφής {dataloaded.poso}€</li>
+              )}
+              )
+            </ul>
+            Αριθμός Αποδειξης:
+            {dataloaded.apodeixi}{" "}
+          </p>
+          <div>
+            <h3>Yπογραφή</h3>
+            <img src="/sfragida.png" alt="σφαραγίδα" width="200" />
+          </div>
+          <h3>ΒΕΒΑΙΩΝΕΤΑΙ ΟΤΙ:</h3>{" "}
+          <ul className="telos">
+            <li>Η ΣΥΝΤΑΓΗ ΤΟΥ ΙΑΤΡΟΥ ΕΚΤΕΛΕΣΤΗΚΕ ΕΠΑΚΡΙΒΩΣ</li>
+            <li>
+              ΤΑ ΓΥΑΛΙΑ ΟΡΑΣΕΩΣ,ΣΚΕΛΕΤΟΣ ΚΑΙ ΟΦΘΑΛΜΙΚΟΙ ΦΑΚΟΙ,ΟΠΩΣ ΚΑΙ ΟΙ ΦΑΚΟΙ
+              ΕΠΑΦΗΣ ΕΙΝΑΙ ΠΡΟΪΟΝΤΑ ΣΥΜΦΩΝΑ ΜΕ ΤΙΣ ΔΙΑΤΑΞΕΙΣ ΤΗΣ ΕΥΡΩΠΑΪΚΗΣ
+              ΕΝΩΣΗΣ ΚΑΙ ΠΛΗΡΟΥΝ ΤΟΥ ΚΑΝΟΝΕΣ ΚΑΙ ΤΟΥΣ ΟΡΟΥΣ ΤΗΣ ΣΦΡΑΓΙΔΑΣ
+              ΠΙΣΤΟΠΟΙΗΣΗΣ <span>CE</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Bebaiosi.js
@@ -0,0 +1,97 @@
+import React, { useRef, useState } from "react";
+
+function Bebaiosi(props) {
+  const [data, setdata] = useState([]);
+
+  const nameRef = useRef();
+  const dayRef = useRef();
+  const apodixiRef = useRef();
+  const amkaRef = useRef();
+  const asfalismenosRef = useRef();
+  const doctorRef = useRef();
+  const tameioRef = useRef();
+  const posoRef = useRef();
+  const selectedRef = useRef();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const enteredName = nameRef.current.value;
+    const enteredDay = dayRef.current.value;
+    const enteredApodixi = apodixiRef.current.value;
+    const enteredPoso = posoRef.current.value;
+    const enteredAMKA = amkaRef.current.value;
+    const enteredAsfalismenos = asfalismenosRef.current.value;
+    const enteredDoctor = doctorRef.current.value;
+    const enteredTameio = tameioRef.current.value;
+    const clickedSelectedRef = selectedRef.current.value;
+
+    const data = {
+      optikos: enteredName,
+      day: enteredDay,
+      doctor: enteredDoctor,
+      asfalismenos: enteredAsfalismenos,
+      amka: enteredAMKA,
+      apodeixi: enteredApodixi,
+      poso: enteredPoso,
+      tameio: enteredTameio,
+      selected: clickedSelectedRef,
+    };
+    setdata(data);
+    props.onAddData(data);
+    // window.print();
+
+    // console.table(bebaiosi);
+    // alert(JSON.stringify(bebaiosi));
+  };
+
+  return (
+    <>
+      <div>
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            name="name"
+            ref={nameRef}
+            placeholder="onoma optikou"
+          />
+          <input type="text" name="day" ref={dayRef} placeholder="ημερομηνία" />
+          <input
+            type="text"
+            name="doctor"
+            ref={doctorRef}
+            placeholder="ιατρος"
+          />
+          <input
+            type="text"
+            name="asfalismenos"
+            ref={asfalismenosRef}
+            placeholder="onomateponimo "
+          />
+          <input
+            type="text"
+            name="tameio"
+            ref={tameioRef}
+            placeholder=" ασφαλιστικό ταμείο "
+          />
+          <input type="number" name="amka" ref={amkaRef} placeholder="AMKA " />
+          <select ref={selectedRef}>
+            <option value="1">Γυαλιά Οράσεως Μακρινά</option>
+            <option value="2">Γυαλιά Οράσεως Κοντινά</option>
+            <option value="3">Φακοί Επαφής</option>
+          </select>
+          <input
+            type="number"
+            name="a.apodixis"
+            ref={apodixiRef}
+            placeholder="Aρ. Αποδειξης "
+          />
+          <input type="number" name="poso" ref={posoRef} placeholder="ποσό " />
+          <input type="submit" value="Save" />
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default Bebaiosi;
